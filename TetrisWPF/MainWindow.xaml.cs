0eacef4cdd86d2288deb8ab22003da9c7a20f4d1
Mike-The-Coder <97619742+Mike-The-Coder@users.noreply.github.com>
2022-06-28 10:04:09 -0400

    First Commit
    First Commit

---
+++ TetrisWPF/MainWindow.xaml.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TetrisWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ImageSource[] titleImages = new ImageSource[]
+        {
+            new BitmapImage(new Uri("Assets/TileEmpty.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileCyan.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileBlue.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileOrange.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileYellow.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileGreen.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TilePurple.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileRed.png", UriKind.Relative))
+        };
+
+        private readonly ImageSource[] blockImages = new ImageSource[]
+        {
+
+            new BitmapImage(new Uri("Assets/Block-Empty.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-I.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-J.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-L.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-O.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-S.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-T.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-Z.png", UriKind.Relative)),
+        };
+
+        private readonly Image[,] imageControls;
+
+        private GameState gamestate = new GameState();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            imageControls = SetupGameCanvas(gamestate.GameGrid);
+        }
+
+        private Image[,] SetupGameCanvas(GameGrid grid)
+        {
+            Image[,] imageControls = new Image[grid.Rows, grid.Columns];
+            int cellSize = 25;
+
+            for (int r = 0; r < grid.Rows; r++)
+            {
+                for (int c = 0; c < grid.Columns; c++)
+                {
+                    Image imageControl = new Image
+                    {
+                        Width = cellSize,
+                        Height = cellSize
+                    };
+
+                    Canvas.SetTop(imageControl, (r - 2) * cellSize);
+                    Canvas.SetLeft(imageControl, c * cellSize);
+                    GameCanvas.Children.Add(imageControl);
+                    imageControls[r, c] = imageControl;
+                }
+            }
+            return imageControls;
+        }
+
+        private void DrawGrid(GameGrid grid)
+        {
+            for (int r = 0; r < grid.Rows; r++)
+            {
+                for (int c = 0; c < grid.Columns; c++)
+                {
+                    int id = grid[r, c];
+                    imageControls[r, c].Source = titleImages[id];
+                }
+            }
+        }
+
+        private void DrawBlock(Block block)
+        {
+            foreach (Position p in block.TilePositions())
+            {
+                imageControls[p.Row, p.Column].Source = titleImages[block.Id];
+            }
+        }
+
+        private void Draw(GameState gamestate)
+        {
+            DrawGrid(gamestate.GameGrid);
+            DrawBlock(gamestate.CurrentBlock);
+        }
+
+        private async Task GameLoop()
+        {
+            Draw(gamestate);
+
+            while (!gamestate.GameOver)
+            {
+                await Task.Delay(500);
+                gamestate.MoveBlockDown();
+                Draw(gamestate);
+            }
+
+            GameOverMenu.Visibility = Visibility.Visible;
+        }
+
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (gamestate.GameOver)
+            {
+                return;
+            }
+
+            switch (e.Key)
+            {
+                case Key.Left:
+                    gamestate.MoveBlockLeft();
+                    break;
+                case Key.Right:
+                    gamestate.MoveBlockRight();
+                    break;
+                case Key.Down:
+                    gamestate.MoveBlockDown();
+                    break;
+                case Key.Up:
+                    gamestate.RotateBlockCW();
+                    break;
+                case Key.Z:
+                    gamestate.RotateBlockCCW();
+                    break;
+                    default:
+                        return;
+            }
+            Draw(gamestate);
+        }
+
+        private async void Window_KeyDown(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private async void GameCanvas_Loaded(object sender, RoutedEventArgs e)
+        {
+            await GameLoop();
+        }
+
+        private async void PlayAgain_Click(object sender, RoutedEventArgs e)
+        {
+            gamestate = new GameState();
+            GameOverMenu.Visibility = Visibility.Hidden;
+            await GameLoop();
+        }
+    }
+}
